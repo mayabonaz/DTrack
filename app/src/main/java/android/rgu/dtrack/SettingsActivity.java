a8feaaac49576dbe50afc72a0ced2b13550b2b86
Muhayyo <muhayyo@hotmail.com>
2019-11-10 18:15:58 +0000

    add settings screen UI

--- app/src/main/java/android/rgu/dtrack/SettingsActivity.java
+++ app/src/main/java/android/rgu/dtrack/SettingsActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,20 +13,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
-                .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
+
 }
