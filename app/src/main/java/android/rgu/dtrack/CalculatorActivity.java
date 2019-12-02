4c54550e3c9c0c95e97071341a4eed2096c36a5e
Muhayyo <muhayyo@hotmail.com>
2019-12-02 01:10:30 +0000

    add Calculator screen UI

--- app/src/main/java/android/rgu/dtrack/CalculatorActivity.java
+++ app/src/main/java/android/rgu/dtrack/CalculatorActivity.java
@@ -1,6 +1,7 @@
 package android.rgu.dtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        // add toolbar to calculator screen
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
