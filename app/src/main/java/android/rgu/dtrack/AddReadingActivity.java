58ff2d2e6caac36335e1cc6794e8255cb24c0229
Muhayyo <muhayyo@hotmail.com>
2019-12-04 16:40:14 +0000

    Merge branch 'master' of C:\Users\muhay\AndroidStudioProjects\DTrack with conflicts.

--- app/src/main/java/android/rgu/dtrack/AddReadingActivity.java
+++ app/src/main/java/android/rgu/dtrack/AddReadingActivity.java
@@ -1,8 +1,17 @@
 package android.rgu.dtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+/**
+ * Implementation for the Add Blood test record screen
+ */
+
 
 public class AddReadingActivity extends AppCompatActivity {
 
@@ -10,5 +19,13 @@ public class AddReadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reading);
+
+        // add toolbar to calculator screen
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
